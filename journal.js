0cf034aa872af123edbbea1e7dea05759a28571d
Monicastf <monicas.f555@gmail.com>
2024-11-27 22:24:40 -0700

    main js

---
+++ journal.js
@@ -0,0 +1,19 @@
+june.addEventListener('click', function(){
+console.log("june picture works");
+document.getElementById("images").src = "cafe.jpg";
+})
+
+august.addEventListener('click', function(){
+console.log("august picture works");
+document.getElementById("images").src = "picnic.jpg";
+})
+
+september.addEventListener('click', function(){
+console.log("september picture works");
+document.getElementById("images").src = "stroll.jpg";
+})
+
+october.addEventListener('click', function(){
+console.log("october picture works");
+document.getElementById("images").src = "sushi.jpg";
+})
