f59e245b2186498ef3d11759a04be6488c38c18b
Monicastf <monicas.f555@gmail.com>
2024-11-27 22:44:28 -0700

    idk

--- journal.js
+++ journal.js
@@ -1,19 +1,23 @@
 june.addEventListener('click', function(){
 console.log("june picture works");
 document.getElementById("images").src = "cafe.jpg";
+description.innerHTML = "In this photo I was at the Daydream Coffee cafe with my friend"
 })
 
 august.addEventListener('click', function(){
 console.log("august picture works");
 document.getElementById("images").src = "picnic.jpg";
+description.innerHTML = "I went on a picnic with my classmates at Prince Island Park"
 })
 
 september.addEventListener('click', function(){
 console.log("september picture works");
 document.getElementById("images").src = "stroll.jpg";
+description.innerHTML = "Before my friend left to go back to school in another city, we went on a stroll together in Downtown"
 })
 
 october.addEventListener('click', function(){
 console.log("october picture works");
 document.getElementById("images").src = "sushi.jpg";
+description.innerHTML = "For my birthday in October, I went to eat dinner at Ari Sushi (the best sushi place in Calgary)"
 })
